520e3e44b0234551f0fe9a4e2cd36f5c6953210d
Allef Melo <38713912+HornlessMoose@users.noreply.github.com>
2018-08-21 20:26:09 -0300

    Delete Advogado.java

--- src/isoccer/Advogado.java
+++
@@ -1,8 +0,0 @@
-package isoccer;
-
-public class Advogado extends Funcionario {
-
-    public Advogado(String nome, String email, String CPF, String telefone, double salario){
-        super(nome, email, CPF, telefone, salario);
-    }
-}
